b8e0345e46100bc7cd0384504549853db7428a23
Ulysse Pennetier <upennetier@gmail.com>
2026-02-19 11:20:55 +0100

    feat(seance_6): localisation (translations, currencies, dates, etc)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(TranslatorInterface $translator): Response
     {
         return $this->render('home/index.html.twig', [
-            'message' => 'Hello world',
+            'message' => 'Hello world'
         ]);
     }
 }
